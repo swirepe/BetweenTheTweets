5a27c52504a3085c782955523651f7e8ae08c888
Peter Swire <swirepe@gmail.com>
2012-12-26 00:15:40 -0500

    added some comments

--- stories.py
+++ stories.py
@@ -1,17 +1,30 @@
+#!/usr/bin/python
+
+### special thanks to Batitia, who helped me regain a handle on nltk
+### on Christmas day.  (she's a PhD student in Computational Lingustics,
+### and a really good sport: @snatiabir )
+
+
 from BeautifulSoup import BeautifulSoup
 import glob
 import sys
+## make sure you run nltk.download() and get the punkt package
 import nltk
 
-#td align="left" width="535"
 
+
+## run `getlit.sh` first if you want to play along
 files = glob.glob("./literotica.com/storyxs/stories/*.shtml")
 
 def getTexts(files):
     for f in files:
         rawtext = open(f).read()
         soup = BeautifulSoup(rawtext, convertEntities=BeautifulSoup.HTML_ENTITIES)
-        
+
+
+        ## this seems to be the body of text, 
+        ## but other non-story elements have the same structure
+        #td align="left" width="535" 
         biguns = soup.findAll("td", width="535")
         
         maxtex = ''
@@ -24,9 +37,8 @@ def getTexts(files):
                 maxtext = bigun.getText().replace("  ", '')
                 
                 
-        yield "\n%\n".join([x.replace("\r\n", '') for x in nltk.sent_tokenize(maxtext)])
-        sys.exit(0)
-        
+        yield f, "\n%\n".join([x.replace("\r\n", '') for x in nltk.sent_tokenize(maxtext)])
+
         
 
 getTexts(files)
